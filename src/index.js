2cb49ccbea118d977e6aec5d4d5e7301131e1bc8
JBWERE-coder <lutherwere@gmail.com>
2023-06-24 22:44:05 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector("#create-task-form");
+  form.addEventListener("submit", createNewTask);
 });
+
+const createNewTask = event => {
+  event.preventDefault();
+
+  const newTaskDescription = document.querySelector("#new-task-description");
+  const newTask = createTaskElement(newTaskDescription.value);
+  appendNewTask(newTask);
+
+  event.target.reset();
+};
+
+const createTaskElement = taskDescription => {
+  const taskElement = document.createElement("li");
+  taskElement.textContent = taskDescription;
+  return taskElement;
+};
+
+const appendNewTask = task => {
+  const taskList = document.querySelector("#tasks");
+  taskList.appendChild(task);
+};
